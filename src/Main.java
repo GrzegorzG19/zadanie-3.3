b30ec0705146e5e94afeb3edfe995e404a07e218
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-10 15:15:25 +0200

    Domowe

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    int cyfra;
+    isEven klasa = new isEven();
+    klasa.isEven(2);
+    klasa.isOdd(3);
+    klasa.circleField(4);
+    klasa.potega(10);
+    System.out.printf("%b \n",klasa.isEven(2));
+    System.out.printf("%b \n",klasa.isOdd(3));
+    System.out.printf("%.2f \n",klasa.circleField(4));
+    System.out.printf("%d \n",klasa.potega(10));
+
+    }
+
+
+    }
---
+++ src/isEven.java
@@ -0,0 +1,20 @@
+public class isEven {
+    boolean isEven(int liczba){
+
+
+        return ((liczba % 2) == 0);
+    }
+
+    boolean isOdd(int liczba2){
+
+
+        return ((liczba2 % 2) != 0);
+    }
+
+    Double circleField(double liczba3){
+        return (liczba3 * liczba3 * 3.14);
+    }
+    int potega(int liczba4){
+        return (liczba4 * liczba4);
+    }
+}
