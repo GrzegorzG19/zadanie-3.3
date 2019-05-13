476c88486fccb6c908e41664c5694f169193ea37
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-13 18:07:44 +0200

    Poprawa

--- src/Math.java
+++ src/Math.java
@@ -1,20 +1,21 @@
-public class isEven {
-    boolean isEven(int liczba){
+public class Math {
+    boolean isEven(int liczba) {
 
 
         return ((liczba % 2) == 0);
     }
 
-    boolean isOdd(int liczba2){
+    boolean isOdd(int liczba2) {
 
 
         return ((liczba2 % 2) != 0);
     }
 
-    Double circleField(double liczba3){
+    double circleField(double liczba3) {
         return (liczba3 * liczba3 * 3.14);
     }
-    int potega(int liczba4){
+
+    int potega(int liczba4) {
         return (liczba4 * liczba4);
     }
 }
